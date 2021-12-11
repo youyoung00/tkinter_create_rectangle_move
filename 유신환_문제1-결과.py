3adf9279cd6d6f70d0467d2cc45a1368ab5c2b09
youyoung00 <you889@hanmail.net>
2021-12-11 23:58:06 +0900

    파이선 과제 1 코드추가

--- 유신환_문제1-결과.py
+++ 유신환_문제1-결과.py
@@ -3,24 +3,70 @@ window = Tk()
 window.title("2021-2학기 SW프로그래밍입문 메인과제2-1")
 w = Canvas(window, width=500, height=500)
 w.pack()
-w.create_rectangle(50, 50, 100, 100, fill="yellow")
+left = 50
+top = 50
+right = 100
+bottom = 100
+
+
+w.create_rectangle(left, top, right, bottom, fill="yellow")
 
 def movRight():
-    w.create_rectangle(55, 50, 105, 100, fill="yellow")
+    w.delete(ALL)
+    global left
+    global top
+    global right
+    global bottom
+    
+    left = left + 5
+    right = right + 5
+
+    w.create_rectangle(left, top, right, bottom, fill="yellow")
     
 def movLeft():
-    w.create_rectangle(50, 50, 100, 100, fill="yellow")
+    w.delete(ALL)
+    global left
+    global top
+    global right
+    global bottom
+    
+    left = left - 5
+    right = right - 5
+    
+    w.create_rectangle(left, top, right, bottom, fill="yellow")
     
 def movTop():
-    w.create_rectangle(50, 50, 100, 100, fill="yellow")
+    w.delete(ALL)
+    global left
+    global top
+    global right
+    global bottom
+    
+    top = top - 5
+    bottom = bottom - 5
+
+    w.create_rectangle(left, top, right, bottom, fill="yellow")
     
 def movBottom():
-    w.create_rectangle(50, 50, 100, 100, fill="yellow")
+    w.delete(ALL)
+    global left
+    global top
+    global right
+    global bottom
     
-Button(window, text="<==(좌)", bg="red",fg="black", command=movLeft).pack(side=LEFT)
-Button(window, text="==>(우)", bg="green", fg="black", command=movRight).pack(side=LEFT)
-Button(window, text="^(상)", bg="orange", fg="black", command=movTop).pack(side=LEFT)
-Button(window, text="v(하)", bg="orange", fg="black", command=movBottom).pack(side=LEFT)
+    top = top + 5
+    bottom = bottom + 5
+
+    w.create_rectangle(left, top, right, bottom, fill="yellow")
+    
+Button(window, text="<==(좌)", bg="red",fg="black",
+       command=movLeft).pack(side=LEFT)
+Button(window, text="==>(우)", bg="green", fg="black",
+       command=movRight).pack(side=LEFT)
+Button(window, text="^(상)", bg="orange", fg="black",
+       command=movTop).pack(side=LEFT)
+Button(window, text="v(하)", bg="orange", fg="black",
+       command=movBottom).pack(side=LEFT)
 
 
 window.mainloop()
